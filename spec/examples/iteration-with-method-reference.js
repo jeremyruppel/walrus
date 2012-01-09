a3d1ef16baf56898da949e214d1abea9a1d39bfe
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-01-09 14:37:46 -0500

    Add failing example for broken path nodes

---
+++ spec/examples/iteration-with-method-reference.js
@@ -0,0 +1,18 @@
+{
+  client_hash : function( slug ){ return '#/' + slug; },
+  clients :
+  [
+    {
+      name : 'Nike',
+      slug : 'nike'
+    },
+    {
+      name : 'Audi',
+      slug : 'audi'
+    },
+    {
+      name : 'Old Navy',
+      slug : 'old-navy'
+    }
+  ]
+}
