0511216ebc5eaf56effca80fca12aeddd42fe68a
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-02-02 23:24:27 -0800

    toLocaleString? come on now

--- bin/walrus.collections.min.js
+++ bin/walrus.collections.min.js
@@ -1,6 +1,6 @@
 /**
  * Walrus.js 0.1.1
- * Thu Feb 02 2012 23:21:10 GMT-0800 (PST)
+ * Thu Feb 02 2012
  * (c) 2012 Jeremy Ruppel
  * Walrus.js is freely distributable under the terms of the MIT license.
  * https://raw.github.com/jeremyruppel/walrus/master/LICENSE
