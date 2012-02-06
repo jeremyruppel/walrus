5b235abd95bccbd7c7b9ddf3f0fdc60ec4fea48b
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-02-06 11:00:09 -0800

    Document existing strings filters

--- bin/walrus.strings.js
+++ bin/walrus.strings.js
@@ -10,17 +10,36 @@
 
   Walrus = (typeof require !== "undefined" && require !== null) && (typeof exports !== "undefined" && exports !== null) ? require('./walrus') : window.Walrus;
 
-  /*
-  TODO
-  - Write :strip
-  - Move :upcase here
-  - Move :downcase here
-  - Write :blank?
+  /**
+   * *:squish*
+   * Removes all leading and trailing whitespace, then condenses any internal whitespace
+   * groups to a single space each.
+   *
+   * Parameters: none
+   *
+   * Usage:
+   *
+   *  {{ " foo    bar \n baz"  | :squish }} // => "foo bar baz"
   */
 
   Walrus.Filters.add('squish', function(string) {
-    return string.replace(/\s+/g, ' ').replace(/^\s+|\s+$/g, '');
+    return string.replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ');
   });
+
+  /**
+   * *:truncate*
+   * Truncates a string to the specified `length`, optionally breaking at a separator
+   * or providing a custom string to denote an omission.
+   *
+   * Parameters:
+   *  length (int) - the number of characters to keep, at most
+   *  separator (string) - Optional: a specified string to break on
+   *  omission (string) - Optional: a string to denote an omission
+   *
+   * Usage:
+   *
+   *  {{ "Once upon a time in a world far far away" | :truncate( 27, ' ' ) }} // => "Once upon a time in a..."
+  */
 
   Walrus.Filters.add('truncate', function(string, length, separator, omission) {
     var chars, stop;
