56d65572c6b9df108aaf7577fd9d03f053534ae0
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-02-06 12:31:05 -0800

    Add an example for climbing the $parent tree

---
+++ test/examples/iteration-with-multiple-parents.js
@@ -0,0 +1,42 @@
+{
+  "result" : 
+  {
+    "artist" : "Creedence Clearwater Revival",
+    "albums" :
+    [
+      {
+        "title" : "Bayou Country",
+        "songs" :
+        [
+          {
+            "title" : "Proud Mary"
+          },
+          {
+            "title" : "Good Golly Miss Molly"
+          }
+        ]
+      },
+      {
+        "title" : "Green River",
+        "songs" :
+        [
+          {
+            "title" : "Bad Moon Rising"
+          }
+        ]
+      },
+      {
+        "title" : "Willy and the Poor Boys",
+        "songs" :
+        [
+          {
+            "title" : "Down on the Corner"
+          },
+          {
+            "title" : "Fortunate Son"
+          }
+        ]
+      }
+    ]
+  }
+}
