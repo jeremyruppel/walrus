d1834ba95aef8fa7bf0e2929a13182aecd59a60a
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-02-06 11:22:07 -0800

    Document the collections filters

--- bin/walrus.collections.js
+++ bin/walrus.collections.js
@@ -10,12 +10,22 @@
 
   Walrus = (typeof require !== "undefined" && require !== null) && (typeof exports !== "undefined" && exports !== null) ? require('./walrus') : window.Walrus;
 
-  /*
-  TODO
-  - More enumerable stuff?
-  - Write :any?/:empty?
-  - Write :group_by
-  - Write :pluck
+  /**
+   * *:first*
+   * Selects the first _count_ items of the array. Defaults to only the first item.
+   *
+   * Parameters:
+   *  count - Optional: how many items to include
+   *
+   * Usage:
+   *
+   *  {{ :each numbers | :first do}}
+   *    // 1
+   *  {{ end }}
+   *
+   *  {{ :each numbers | :first( 5 ) do }}
+   *    // 1 2 3 4 5
+   *  {{ end }}
   */
 
   Walrus.Filters.add('first', function(array, count) {
@@ -26,6 +36,24 @@
     }
   });
 
+  /**
+   * *:last*
+   * Selects the last _count_ items of the array. Defaults to only the last item.
+   *
+   * Parameters:
+   *  count - Optional: how many items to include
+   *
+   * Usage:
+   *
+   *  {{ :each numbers | :last do}}
+   *    // 10
+   *  {{ end }}
+   *
+   *  {{ :each numbers | :last( 5 ) do }}
+   *    // 6 7 8 9 10
+   *  {{ end }}
+  */
+
   Walrus.Filters.add('last', function(array, count) {
     if (count != null) {
       return array.slice(-count);
@@ -34,13 +62,52 @@
     }
   });
 
+  /**
+   * *:count*
+   * Returns the length of the given array.
+   *
+   * Parameters: none
+   *
+   * Usage:
+   *
+   *  var numbers = [ 1, 2, 3, 4, 5 ];
+   *
+   *  {{ numbers | :count }} // => 5
+  */
+
   Walrus.Filters.add('count', function(array) {
     return array.length;
   });
 
+  /**
+   * *:any*
+   * Returns true if the array is not empty. Opposite of `:empty`.
+   *
+   * Parameters: none
+   *
+   * Usage:
+   *
+   *  var numbers = [ 1, 2, 3, 4, 5 ];
+   *
+   *  {{ numbers | :any }} // => true
+  */
+
   Walrus.Filters.add('any', function(array) {
     return array.length > 0;
   });
+
+  /**
+   * *:empty*
+   * Returns true of the array is empty. Opposite of `:any`.
+   *
+   * Parameters: none
+   *
+   * Usage:
+   *
+   *  var numbers = [ 1, 2, 3, 4, 5 ];
+   *
+   *  {{ numbers | :empty }} // => false
+  */
 
   Walrus.Filters.add('empty', function(array) {
     return array.length === 0;
--- lib/walrus.collections.coffee
+++ lib/walrus.collections.coffee
@@ -1,23 +1,85 @@
 Walrus = if require? and exports? then require './walrus' else window.Walrus
 
+###*
+ * *:first*
+ * Selects the first _count_ items of the array. Defaults to only the first item.
+ *
+ * Parameters:
+ *  count - Optional: how many items to include
+ *
+ * Usage:
+ *
+ *  {{ :each numbers | :first do}}
+ *    // 1
+ *  {{ end }}
+ *
+ *  {{ :each numbers | :first( 5 ) do }}
+ *    // 1 2 3 4 5
+ *  {{ end }}
 ###
-TODO
-- More enumerable stuff?
-- Write :any?/:empty?
-- Write :group_by
-- Write :pluck
-###
-
 Walrus.Filters.add 'first', ( array, count ) ->
 
   if count? then array.slice 0, count else array[ 0 ]
 
+###*
+ * *:last*
+ * Selects the last _count_ items of the array. Defaults to only the last item.
+ *
+ * Parameters:
+ *  count - Optional: how many items to include
+ *
+ * Usage:
+ *
+ *  {{ :each numbers | :last do}}
+ *    // 10
+ *  {{ end }}
+ *
+ *  {{ :each numbers | :last( 5 ) do }}
+ *    // 6 7 8 9 10
+ *  {{ end }}
+###
 Walrus.Filters.add 'last', ( array, count ) ->
 
   if count? then array.slice -count else array[ array.length - 1 ]
 
+###*
+ * *:count*
+ * Returns the length of the given array.
+ *
+ * Parameters: none
+ *
+ * Usage:
+ *
+ *  var numbers = [ 1, 2, 3, 4, 5 ];
+ *
+ *  {{ numbers | :count }} // => 5
+###
 Walrus.Filters.add 'count', ( array ) -> array.length
 
+###*
+ * *:any*
+ * Returns true if the array is not empty. Opposite of `:empty`.
+ *
+ * Parameters: none
+ *
+ * Usage:
+ *
+ *  var numbers = [ 1, 2, 3, 4, 5 ];
+ *
+ *  {{ numbers | :any }} // => true
+###
 Walrus.Filters.add 'any', ( array ) -> array.length > 0
 
+###*
+ * *:empty*
+ * Returns true of the array is empty. Opposite of `:any`.
+ *
+ * Parameters: none
+ *
+ * Usage:
+ *
+ *  var numbers = [ 1, 2, 3, 4, 5 ];
+ *
+ *  {{ numbers | :empty }} // => false
+###
 Walrus.Filters.add 'empty', ( array ) -> array.length is 0
