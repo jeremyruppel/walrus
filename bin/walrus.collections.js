e493f200782d8d3c84d05af60c8f4b15efb2a6b0
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-02-21 11:18:43 -0800

    Version bump

--- bin/walrus.collections.js
+++ bin/walrus.collections.js
@@ -1,5 +1,5 @@
 /**
- * Walrus.js 0.4.4
+ * Walrus.js 0.5.0
  * (c) 2012 Jeremy Ruppel
  * Walrus.js is freely distributable under the terms of the MIT license.
  * https://raw.github.com/jeremyruppel/walrus/master/LICENSE
